9c06373775f8bc523cd9d1718b1c132a4dbaa6ed
oldstuden <atikin3002@gmail.com>
2023-06-30 21:35:39 +0200

    continued task07

--- js/07_task1.js
+++ js/07_task1.js
@@ -1,36 +1,62 @@
 const refs = {
-  form: document.querySelector("#task-form"),
-  taskList: document.querySelector("#task-list"),
+  form: document.querySelector('#task-form'),
+  taskList: document.querySelector('#task-list'),
 };
-const NAME_KEY = "input_value";
+const NAME_KEY = 'input_value';
 
 const { form, taskList } = refs;
-
+const arr = [];
+// const id = Math.random();
+// arr.push({ name, id });
+// const tasks = JSON.parse(localStorage.getItem(NAME_KEY));
+// const filteredTasks = tasks.filter((task) => task.id !== id);
 render();
 
-form.addEventListener("submit", onSubmit);
-
+form.addEventListener('submit', onSubmit);
+taskList.addEventListener('click', onClick);
 function onSubmit(event) {
   event.preventDefault();
   const task = event.target.elements.taskName.value.trim();
   if (!task) return;
-  saveData(task);
+  arr.push(task);
+  saveData(arr);
+  const dataFromLocalStorage = JSON.parse(localStorage.getItem(NAME_KEY));
+  if (!dataFromLocalStorage) return;
+  createMarkup(dataFromLocalStorage);
+  event.target.reset();
 }
 
 function saveData(data) {
-  localStorage.setItem(NAME_KEY, data);
+  localStorage.setItem(NAME_KEY, JSON.stringify(data));
 }
 
 function createMarkup(data) {
-  const line = `<li>
-        <span>${data}</span>
+  console.log(data);
+  const line = data
+    .map(
+      (elem) =>
+        `<li>
+        <span>${elem}</span>
         <button type="button">Delete</button>
-    </li>`;
-  taskList.insertAdjacentHTML("beforeend", line);
+    </li>`
+    )
+    .join('');
+  console.log(line);
+  taskList.innerHTML = '';
+  taskList.insertAdjacentHTML('beforeend', line);
 }
 
 function render() {
-  const dataFromLocalStorage = localStorage.getItem(NAME_KEY);
+  const dataFromLocalStorage = JSON.parse(localStorage.getItem(NAME_KEY));
   if (!dataFromLocalStorage) return;
+  createMarkup(dataFromLocalStorage);
+}
+function onClick(evt) {
+  const textValue = evt.target.previousElementSibling.textContent;
+  const dataFromLocalStorage = JSON.parse(localStorage.getItem(NAME_KEY));
+  if (!dataFromLocalStorage) return;
+  const idx = dataFromLocalStorage.indexOf(textValue);
+  dataFromLocalStorage.splice(idx, 1);
+  saveData(dataFromLocalStorage);
   createMarkup(dataFromLocalStorage);
 }
